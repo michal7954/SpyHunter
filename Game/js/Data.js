4b2ce7f0c438670f197d3dea97263aa5a12d7ae0
Michał Madeja <35845468+michal7954@users.noreply.github.com>
2018-06-01 11:31:43 +0200

    Sterowanie

--- Game/js/Data.js
+++ Game/js/Data.js
@@ -12,6 +12,25 @@ function Data() {
     this.area.alt = "area";
 
     this.frame = -65535 + 590;
-    this.speed = 100;
+
+    this.speed = {
+        curr: 0,
+        max: 14,
+        min: 0,
+        acceleration: 0.4,
+        deceleration: -0.7,
+        poz_range: 6,
+        poz_addition: 160,
+    }
+
+    this.poz = {
+        x: 200,
+        y: 238
+    }
+
+    this.map = {
+        player: [21, 23, 13, 23],
+        speedy: [45, 23, 13, 31],
+    }
 
 }
--- Game/js/Main.js
+++ Game/js/Main.js
@@ -2,16 +2,46 @@ var data;
 var events;
 
 window.onload = function () {
+
     data = new Data();
     events = new Events();
     render();
 };
 
-
 function render() {
-    data.ctx.drawImage(data.area, 0, data.frame, 480, 65535);
-    data.frame = data.frame + data.speed;
 
-    console.log(events)
+    // MAPA
+    data.ctx.drawImage(data.area, 0, data.frame, 480, 65535);
+    data.frame = data.frame + data.speed.curr;
+
+
+    // STEROWANIE GRACZEM
+    if (events.up && data.speed.curr <= data.speed.max) {
+        data.speed.curr = data.speed.curr + data.speed.acceleration;
+        data.poz.y = (data.speed.max - data.speed.curr) * data.speed.poz_range + data.speed.poz_addition
+
+        data.ctx.drawImage(data.elements, data.map.speedy[0], data.map.speedy[1], data.map.speedy[2], data.map.speedy[3], data.poz.x, data.poz.y, data.map.speedy[2], data.map.speedy[3]);
+    }
+    else {
+        data.ctx.drawImage(data.elements, data.map.player[0], data.map.player[1], data.map.player[2], data.map.player[3], data.poz.x, data.poz.y, data.map.player[2], data.map.player[3]);
+    }
+
+    if (events.down) {
+        if (data.speed.curr + data.speed.deceleration < data.speed.min) {
+            data.speed.curr = data.speed.min;
+        }
+        else {
+            data.poz.y = (data.speed.max - data.speed.curr) * data.speed.poz_range + data.speed.poz_addition;
+            data.speed.curr = data.speed.curr + data.speed.deceleration
+        }
+    }
+
+    if (events.left) {
+        data.poz.x = data.poz.x - 4;
+    }
+    if (events.right) {
+        data.poz.x = data.poz.x + 4;
+    }
+
     requestAnimationFrame(render);
 };
