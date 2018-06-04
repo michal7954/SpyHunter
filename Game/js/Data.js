3587a15d4ac6465ffdabb111c8ac3de2015cf8f8
Michał Madeja <35845468+michal7954@users.noreply.github.com>
2018-06-04 17:45:17 +0200

    Kolizje z mapą

--- Game/js/Data.js
+++ Game/js/Data.js
@@ -15,22 +15,24 @@ function Data() {
 
     this.speed = {
         curr: 0,
-        max: 14,
+        max: 8,
         min: 0,
-        acceleration: 0.4,
-        deceleration: -0.7,
-        poz_range: 6,
-        poz_addition: 160,
+        acceleration: 0.2,
+        deceleration: -0.3,
+        poz_range: 10,
+        poz_addition: 180,
+        turn_radius: 4
     }
 
     this.poz = {
         x: 200,
-        y: 238
+        y: (this.speed.max - this.speed.curr) * this.speed.poz_range + this.speed.poz_addition,
+        trans: 0,
+        r: 4
     }
 
     this.map = {
         player: [21, 23, 13, 23],
         speedy: [45, 23, 13, 31],
     }
-
 }
--- Game/js/Main.js
+++ Game/js/Main.js
@@ -15,15 +15,60 @@ function render() {
     data.frame = data.frame + data.speed.curr;
 
 
+    //KOLIZJE Z MAPĄ
+    var kol = {
+        pl: data.ctx.getImageData(data.poz.x - data.poz.r, data.poz.y - 1, 1, 1).data,
+        pr: data.ctx.getImageData(data.poz.x + data.poz.r + data.map.player[2], data.poz.y - 1, 1, 1).data,
+    }
+
+    kol.wl = !(kol.pl[0] == 120 && kol.pl[1] == 120 && kol.pl[2] == 120 && kol.pl[3] == 255);
+    kol.wr = !(kol.pr[0] == 120 && kol.pr[1] == 120 && kol.pr[2] == 120 && kol.pr[3] == 255);
+
+
+    if (kol.wl && kol.wr) {
+        //bum
+        data.speed.curr = 0;
+    }
+    else if (kol.wl || kol.wr) {
+        //drgawki
+        if (data.frame % 2 == 0) {
+            data.poz.trans = 1;
+        }
+        else {
+            data.poz.trans = -1;
+        }
+    }
+    else {
+        //gut
+        data.poz.trans = 0;
+    }
+
+
+
+    data.ctx.beginPath();
+
+    data.ctx.moveTo(data.poz.x - data.poz.r, data.poz.y);
+    data.ctx.lineTo(data.poz.x - data.poz.r, data.poz.y - 2);
+
+    data.ctx.moveTo(data.poz.x + data.poz.r + data.map.player[2], data.poz.y);
+    data.ctx.lineTo(data.poz.x + data.poz.r + data.map.player[2], data.poz.y - 2);
+
+    data.ctx.lineWidth = 1;
+    data.ctx.strokeStyle = "rgba(0,0,0,1)"
+    data.ctx.stroke();
+
+
+
+
     // STEROWANIE GRACZEM
     if (events.up && data.speed.curr <= data.speed.max) {
         data.speed.curr = data.speed.curr + data.speed.acceleration;
         data.poz.y = (data.speed.max - data.speed.curr) * data.speed.poz_range + data.speed.poz_addition
 
-        data.ctx.drawImage(data.elements, data.map.speedy[0], data.map.speedy[1], data.map.speedy[2], data.map.speedy[3], data.poz.x, data.poz.y, data.map.speedy[2], data.map.speedy[3]);
+        data.ctx.drawImage(data.elements, data.map.speedy[0], data.map.speedy[1], data.map.speedy[2], data.map.speedy[3], data.poz.x + data.poz.trans, data.poz.y, data.map.speedy[2], data.map.speedy[3]);
     }
     else {
-        data.ctx.drawImage(data.elements, data.map.player[0], data.map.player[1], data.map.player[2], data.map.player[3], data.poz.x, data.poz.y, data.map.player[2], data.map.player[3]);
+        data.ctx.drawImage(data.elements, data.map.player[0], data.map.player[1], data.map.player[2], data.map.player[3], data.poz.x + data.poz.trans, data.poz.y, data.map.player[2], data.map.player[3]);
     }
 
     if (events.down) {
@@ -37,11 +82,22 @@ function render() {
     }
 
     if (events.left) {
-        data.poz.x = data.poz.x - 4;
+        data.poz.x = data.poz.x - data.speed.turn_radius;
     }
     if (events.right) {
-        data.poz.x = data.poz.x + 4;
+        data.poz.x = data.poz.x + data.speed.turn_radius;
     }
+
+
+
+
+
+
+
+
+
+
+
 
     requestAnimationFrame(render);
 };
